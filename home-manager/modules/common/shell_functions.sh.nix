c25e640af23c91fcb5c55eb8417a65acf8ffce87
Victor Loh <vic94loh@hotmail.com>
2022-08-13 17:24:28 +0800

    feat: improvements on hm shortcuts

--- home-manager/modules/common/shell_functions.sh.nix
+++ home-manager/modules/common/shell_functions.sh.nix
@@ -3,7 +3,7 @@ let
   NXPKGS_CFG_PATH = "${config.home.homeDirectory}/.config/nixpkgs";
 in
 ''
-function nxup() {
+nxup() {
   local restore=$PWD
   cd ${NXPKGS_CFG_PATH}
   nix-channel --update
@@ -11,41 +11,64 @@ function nxup() {
   cd $restore
 }
 
-function hmapply() {
+hmapply() {
   local restore=$PWD
   cd ${NXPKGS_CFG_PATH}
-  home-manager switch --impure --flake .#$@
+
+  echo "Started applying of home manager profiles, showing possible profile values:"
+  # This is functionally similar to hmls, because hmls isnt available due to the position of aliases definition under home-manager
+  # We cannot use hmls directly.
+  homeManagerProfiles="$(awk '/^###### HOMECONFIG PROFILES START/{p=1;next};/^###### HOMECONFIG PROFILES END/{p=0};p' ~/.config/nixpkgs/flake.nix | awk -F'=' '{print $1}' | awk '{$1=$1;print}')"
+  echo "$homeManagerProfiles"
+  echo ""
+
+  local homeProfileName=""
+  if [ $# -gt 0 ]; then
+    homeProfileName="$1"
+    shift;
+  else
+    echo "please enter a home profile name to apply to via the list shown above, and then press [ENTER] "
+    read homeProfileName
+  fi
+  local checkHomeProfile="$(echo "$homeManagerProfiles" | sed -n "/^$homeProfileName$/p")"
+  if [ -z "$checkHomeProfile" ]; then
+    echo "An invalid home profile name has been specified, the specified name was '$homeProfileName'"
+    return 1
+  fi
+
+  echo "Running home manager profile switch on $homeProfileName now"
+  home-manager switch --impure --flake $@ .#$homeProfileName
   cd $restore
 }
 
 # Simple edit function
-function e() {
+e() {
   # use $EDITOR, otherwise if not found fallback to vim
   local editor="''${EDITOR:-vi}"
   $editor $@
 }
 
-function gf() {
+gf() {
   branch=$1;
   shift;
   git fetch origin -f "$branch":"$branch"
 }
 
-function gnb() {
+gnb() {
   branch_to_checkout=$1;
   shift;
   git checkout "$branch_to_checkout" && git pull && git checkout -b $@
   echo "rest of args is: $@"
 }
 
-function gpretty() {
+gpretty() {
   # Pretty prints between 2 revisions e.g. tag_name against master
   prev_rev=$1;
   current_rev=$2;
   git log --pretty=format:%s%n%b%n----------- --merges --first-parent $prev_rev..$current_rev
 }
 
-function gprunelocal() {
+gprunelocal() {
   # Prunes local branches that have been orphaned (remote deleted etc.)
   # Does a dry run if no args specified, must include a yes to prune for real
   should_prune=$1
@@ -60,7 +83,7 @@ function gprunelocal() {
 ######
 # Rsync and SSH utils
 ######
-function rsynccp() {
+rsynccp() {
   # Uses rsync with the following options -chavzP --stats to copy from remote to local
   # For explanation of what the flags do, check man rsync OR http://explainshell.com/explain?cmd=rsync+-chavzP+--stats+user%40remote.host%3A%2Fpath%2Fto%2Fcopy+%2Fpath%2Fto%2Flocal%2Fstorage
   if [[ $# -ne 2 ]]; then
@@ -72,8 +95,31 @@ function rsynccp() {
   rsync -chavzP --stats $1 $2
 }
 
+## Home manager functions
+hmrollback() {
+  echo "Started rollback of home manager generations, showing generations"
+  home-manager generations
+  echo ""
+
+  local rollbackID=""
+  if [ $# -gt 0 ]; then
+    rollbackID="$1"
+  else
+    echo "please enter a valid ID to rollback to via the generations shown above, and then press [ENTER] "
+    read rollbackID
+  fi
+  local generationPath="$(home-manager generations | awk -F 'id ' '{ print (NF>1) ? $NF : ""}' | sed -n "/^$rollbackID -/p" | awk -F "$rollbackID -> " '{ print (NF>1) ? $NF : ""}')"
+  if [ -z "$generationPath" ]; then
+    echo "An invalid generation ID has been specified, the specified generation was '$rollbackID'"
+    return 1
+  fi
+  echo "Running rollback script now"
+  echo "The path to the rollback script for generation '$rollbackID' is at $generationPath/activate"
+  $generationPath/activate
+}
+
 ## Nix functions
-function nxsh() {
+nxsh() {
   local restore=$PWD
   cd ${NXPKGS_CFG_PATH}
   # nix develop $@
