d2276e8319958cc17599daa34c74519578220269
Victor Loh <vic94loh@hotmail.com>
2022-08-22 21:50:21 +0800

    refactor: split GUI from common to its own module

---
+++ home-manager/modules/gui/browser.nix
@@ -0,0 +1,133 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.customHomeProfile.GUI;
+  # https://nur.nix-community.org/repos/rycee/
+  inherit (pkgs.nur.repos.rycee) firefox-addons;
+
+  ffcommon_settings = {
+    "browser.newtabpage.activity-stream.showSponsored" = false;
+    "browser.tabs.warnOnClose" = false;
+    "extensions.pocket.enabled" = false;
+    "privacy.donottrackheader.enabled" = true;
+    "privacy.trackingprotection.enabled" = true;
+    "privacy.trackingprotection.socialtracking.enabled" = true;
+    "general.autoScroll" = true;
+    "webgl.force-enable" = true;
+  };
+  ffprivate_settings = {
+    "browser.privatebrowsing.autostart" = true;
+    "browser.startup.homepage" = "about:blank";
+    # https://www.eff.org/https-everywhere/set-https-default-your-browser
+    "dom.security.https_only_mode" = true;
+    "privacy.clearOnShutdown.cache" = true;
+    "privacy.clearOnShutdown.cookies" = true;
+    "privacy.clearOnShutdown.downloads" = true;
+    "privacy.clearOnShutdown.formdata" = true;
+    "privacy.clearOnShutdown.history" = true;
+    "privacy.clearOnShutdown.offlineApps" = true;
+    "privacy.clearOnShutdown.openWindows" = true;
+    "privacy.clearOnShutdown.sessions" = true;
+    "privacy.clearOnShutdown.siteSettings" = true;
+    "signon.rememberSignons" = false;
+  };
+
+  custom_firefox_pkg = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+    extraPolicies = {
+      # Policies here: https://github.com/mozilla/policy-templates
+      AppAutoUpdate = false;
+      DisableAppUpdate = true;
+      DisableBuiltinPDFViewer = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisableDeveloperTools = false;
+      DisableFeedbackCommands = true;
+      DisableSystemAddonUpdate = true;
+      DisableFormHistory = true;
+      NewTabPage = false;
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = true;
+      NetworkPrediction = false; # disable dns prefetch
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      PasswordManagerEnabled = false;
+      SearchSuggestEnabled = false;
+      FirefoxHome = {
+        Search = false;
+        Highlights = false;
+        Pocket = false;
+        SponsoredPocket = false;
+        Snippets = false;
+        TopSites = false;
+        SponsoredTopSites = false;
+      };
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        SkipOnboarding = true;
+      };
+      Homepage = {
+        StartPage = "previous-session";
+      };
+    };
+    extraPrefs = ''
+      // Show more ssl cert infos
+      lockPref("security.identityblock.show_extended_validation", true);
+    '';
+  };
+in
+{
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    (lib.mkIf pkgs.stdenv.isLinux {
+      home.file = {
+        ".local/share/applications/firefox.desktop" = {
+          text = ''
+            [Desktop Entry]
+            Categories=Network;WebBrowser
+            Exec=nixGL firefox -P Default %U
+            GenericName=Web Browser
+            Icon=${custom_firefox_pkg}/share/icons/hicolor/128x128/apps/firefox.png
+            MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp
+            Name=Firefox
+            Type=Application
+            Version=1.4
+            Terminal=False
+            Actions=private_window;
+
+            [Desktop Action private_window]
+            Name=Open a Private Window
+            Icon=${custom_firefox_pkg}/share/icons/hicolor/128x128/apps/firefox.png
+            Exec=nixGL firefox -P Private %U
+          '';
+          executable = true;
+        };
+      };
+    })
+    {
+      programs.firefox = {
+        enable = true;
+        # https://nixos.org/manual/nixpkgs/stable/#build-wrapped-firefox-with-extensions-and-policies
+        package = custom_firefox_pkg;
+        extensions = [
+          firefox-addons.ublock-origin
+          firefox-addons.bitwarden
+          firefox-addons.darkreader
+          firefox-addons.decentraleyes
+          firefox-addons.disable-javascript
+          firefox-addons.https-everywhere
+          firefox-addons.privacy-badger
+          firefox-addons.unpaywall
+        ];
+        profiles.default = {
+          name = "Default";
+          settings = ffcommon_settings;
+          isDefault = true;
+        };
+        profiles.private = {
+          id = 1;
+          name = "Private";
+          settings = ffcommon_settings // ffprivate_settings;
+        };
+      };
+    }
+  ]);
+}
---
+++ home-manager/modules/gui/default.nix
@@ -0,0 +1,70 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.customHomeProfile.GUI;
+  # NOTE: *that* an-anime-game-launcher specific, assume that installation is done in linux via the
+  # appropriate channels (not going to link here, but lookup 'n-anime-game-launcher')
+  # The defaults here are usually the same and its *USUALLY* safe to go ahead and delete to reinstall
+  # These settings if we somehow bork the relevant configuration
+  agl_root = "${config.home.homeDirectory}/.local/share/anime-game-launcher";
+in
+{
+  imports = [
+    ./browser.nix
+    ./vscode.nix
+    # ./conky.nix
+  ];
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    (lib.mkIf (pkgs.stdenv.isLinux && cfg.gaming.enable) {
+      home.shellAliases = {
+        genshin_start = ''WINEPREFIX='${agl_root}/game' ${agl_root}/runners/lutris-GE-Proton7-22-x86_64/bin/wine64 ${agl_root}/game/drive_c/Program\ Files/Genshin\ Impact/GenshinImpact.exe'';
+      };
+    })
+    (lib.mkIf pkgs.stdenv.isLinux {
+      home.file = {
+        ".local/share/applications/Alacritty.desktop" = {
+          text = ''
+            [Desktop Entry]
+            Categories=System;TerminalEmulator;
+            Exec=nixGL alacritty
+            GenericName=Terminal
+            Icon=${pkgs.alacritty}/share/icons/hicolor/scalable/apps/Alacritty.svg
+            Name=Alacritty
+            Type=Application
+            Terminal=false
+            Comment=A fast, cross-platform, OpenGL terminal emulator
+            StartupWMClass=Alacritty
+            Actions=New;
+
+            [Desktop Action New]
+            Name=Open a New Terminal
+            Icon=${pkgs.alacritty}/share/icons/hicolor/scalable/apps/Alacritty.svg
+            Exec=nixGL alacritty
+          '';
+          executable = true;
+        };
+      };
+    })
+    {
+      home.packages = [
+        pkgs.nixgl.auto.nixGLDefault
+        pkgs.bitwarden
+        pkgs.teams
+        pkgs.zoom-us
+        pkgs.discord
+        pkgs.thunderbird
+        pkgs.slack
+      ];
+      programs.alacritty = {
+        # Alacritty is a terminal emulator
+        # TODO: add in alacritty settings a la
+        # https://github.com/alacritty/alacritty
+        # Below are some examples on how
+        # https://github.com/davidtwco/veritas/blob/6f2c676a76ef2885c9102aeaea874c361dbcaf61/home/configs/alacritty.nix
+        # https://arslan.io/2018/02/05/gpu-accelerated-terminal-alacritty/
+        # https://pezcoder.medium.com/how-i-migrated-from-iterm-to-alacritty-c50a04705f95
+        enable = true;
+        package = pkgs.alacritty;
+      };
+    }
+  ]);
+}
