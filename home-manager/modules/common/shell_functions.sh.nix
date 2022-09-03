2111629e5ae81929a43b1917409e5e04cd31dd20
Victor Loh <vic94loh@hotmail.com>
2022-09-03 10:52:15 +0800

    feat: read wrapper func for reading secrets

--- home-manager/modules/common/shell_functions.sh.nix
+++ home-manager/modules/common/shell_functions.sh.nix
@@ -180,4 +180,29 @@ in
     }
   EOF
   }
+
+  # POSIX compliant way of reading strings without echoing to prompt.
+  # This function is merely a wrapper over `read`, for help, please refer
+  # to `man read`
+  read_secret()
+  {
+      # Disable echo.
+      stty -echo
+
+      # Set up trap to ensure echo is enabled before exiting if the script
+      # is terminated while echo is disabled.
+      trap 'stty echo' EXIT
+
+      # Read secret.
+      read "$@"
+
+      # Enable echo.
+      stty echo
+      trap - EXIT
+
+      # Print a newline because the newline entered by the user after
+      # entering the passcode is not echoed. This ensures that the
+      # next line of output begins at a new line.
+      echo
+  }
 ''
