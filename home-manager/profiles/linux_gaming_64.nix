bf347b21291775b962d90c6f86faa0e5394afbc9
Victor Loh <vic94loh@hotmail.com>
2024-02-03 14:30:24 +0800

    chore: add mysql to default

--- home-manager/profiles/linux_gaming_64.nix
+++ home-manager/profiles/linux_gaming_64.nix
@@ -34,6 +34,7 @@
     };
     # cloudDevelopment.k8s.enable = true;
     # cloudDevelopment.terraform.enable = true;
+    databases.mariadb.enable = true;
     corsairKeyboardMouseSupport.enable = false;
     # golang.enable = true;
     rust.enable = true;
