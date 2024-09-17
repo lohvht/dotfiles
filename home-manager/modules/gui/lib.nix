d7b9c3f01b9724c2a5ffb95bcd1171e24c7d5b74
Victor Loh <vic94loh@hotmail.com>
2024-09-17 22:41:41 +0800

    chore(OpenGL): fix errs such as unable to utilise HW accel on FF or nextcloud not rendering due to HW accel issues
    Default to mesa for OpenGL with the optional ability to override to nvidia.

--- home-manager/modules/gui/lib.nix
+++ home-manager/modules/gui/lib.nix
@@ -6,21 +6,33 @@ let
 
   nixGLWrapOpts = pkg: { binSuffix ? ""
                        , wrapperSuffix ? ""
+                       , nixGLPackage ? "mesa"
                        ,
-                       }@options: pkgs.runCommand "${pkg.name}-nixgl-wrapper${wrapperSuffix}" { } ''
-    mkdir $out
-    ln -s ${pkg}/* $out
-    rm $out/bin
-    mkdir $out/bin
-    for bin in ${pkg}/bin/*; do
-      wrapped_bin=$out/bin/$(basename $bin)${binSuffix}
-      cat <<EOF >$wrapped_bin
-    #!${pkgs.bash}/bin/bash
-    exec ${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL $bin \$@
-    EOF
-      chmod +x $wrapped_bin
-    done
-  '';
+                       }@options:
+    let
+      # NOTE: For nvidia - will need to install `prime-run` on system - this is usually located in the nvidia-prime package
+      #
+      # for arch based, do the following:
+      #   pacman -S nvidia-prime
+      nixGLRunCommand =
+        if nixGLPackage == "nvidia" then "/usr/bin/prime-run ${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL"
+        else if nixGLPackage == "auto" then "${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL"
+        else "${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel";
+    in
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper${wrapperSuffix}" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+        wrapped_bin=$out/bin/$(basename $bin)${binSuffix}
+        cat <<EOF >$wrapped_bin
+      #!${pkgs.bash}/bin/bash
+      exec ${nixGLRunCommand} $bin \$@
+      EOF
+          chmod +x $wrapped_bin
+        done
+    '';
 in
 {
   inherit nixGLWrap;
