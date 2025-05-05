169729eda447ab6f11200e6deb1ab7aeeeb917bd
Victor Loh <vic94loh@hotmail.com>
2025-05-05 14:56:10 +0800

    chore: cleanup unused desktop configs

--- home-manager/profiles/linux_gaming_64.nix
+++ home-manager/profiles/linux_gaming_64.nix
@@ -9,7 +9,7 @@
   systemHardwareInfo = {
     cpuMake = "ryzen";
     gpus = [{ name = "Radeon RX 5700/5700 XT"; pcie_bus_id = "0b"; pcie_device_id = "00"; driver = "amdgpu"; }];
-    networkInterface = "wlp4s0";
+    networkInterface = "wlan0";
     disks = [
       { name = "System"; mountedPath = "/"; }
       { name = "HDD1"; mountedPath = "${config.home.homeDirectory}/HDD/HDD1"; }
@@ -19,10 +19,6 @@
     systemCtlPath = "/usr/bin/systemctl";
     GUI = {
       enable = true;
-      gaming = {
-        enable = true;
-        animeGameLauncherRunnerName = "lutris-GE-Proton7-35-x86_64";
-      };
       vscode = {
         enable = true;
         crashReporterUUID = "13fcbd5c-0f51-4819-9701-70b7b4e5cf06";
@@ -32,19 +28,10 @@
       communications.slack.enable = true;
       nextcloudClient.enable = true;
     };
-    # cloudDevelopment.k8s.enable = true;
-    # cloudDevelopment.terraform.enable = true;
-    databases.mariadb.enable = true;
     corsairKeyboardMouseSupport.enable = false;
-    # golang.enable = true;
-    rust.enable = true;
-    python.enable = true;
-    # ruby.enable = true;
-    # node.enable = true;
-    latex.enable = true;
     passwordManagers.bitwarden.enable = true;
     blurayCd = {
-      makemkv.enable = true;
+      mkvtoolnix.enable = true;
       handbrake.enable = true;
       abcde.enable = true;
     };
